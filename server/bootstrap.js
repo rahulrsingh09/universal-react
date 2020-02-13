e62e6199670393833c94264df17e724c095ca64b
Rahul <rahsinwb@amazon.com>
2020-02-13 23:34:49 +0530

    Running On server changes to babel

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,10 +1,15 @@
 require('ignore-styles');
 
 require('@babel/register')({
-
     ignore: [ /(node_modules)/ ],
-    presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: ['@babel/plugin-proposal-class-properties']
+    presets: ['@babel/preset-env', '@babel/preset-react', "@babel/preset-typescript"],
+    plugins: ['@babel/plugin-proposal-class-properties'],
+    // Setting this will remove the currently hooked extensions of `.es6`, `.es`, `.jsx`, `.mjs`
+    // and .js so you'll have to add them back if you want them to be used again.
+    extensions: [".es6", ".es", ".tsx", ".js", "jsx",".mjs"],
+
+    // Setting this to false will disable the cache.
+    cache: true,
 
 });
 
